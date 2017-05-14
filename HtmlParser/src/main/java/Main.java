8d7b220e02f9845a1ff486ac099d09d026054a7b
wanghanyu <wanghanyu8@jd.com>
2017-05-14 15:58:38 +0800

    Init Code

--- HtmlParser/src/main/java/Main.java
+++ HtmlParser/src/main/java/Main.java
@@ -1,5 +1,24 @@
+import com.alibaba.fastjson.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * Created by wanghanyu on 2017/4/28.
  */
 public class Main {
+    public static void main(String[] args) {
+        String str = "<div class='aaa' property='{\"name\":\"asd\"}'></div><div></div>";
+        Document doc = Jsoup.parse(str);
+        Elements eles = doc.select("div.aaa");
+//        for(Element ele : eles) {
+//            String property = ele.attr("property");
+//            JSONObject object = JSONObject.parseObject(property);
+//            System.out.println(object.get("name"));
+//        }
+        Element ele = eles.get(0);
+        ele.tagName("ware-title");
+        System.out.println(doc.body().children().toString());
+    }
 }
